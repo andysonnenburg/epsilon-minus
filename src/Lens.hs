337717e7406fea9e26625bbcc0b48e8f33753a32
Andy Sonnenburg <andy.sonnenburg@mathworks.com>
2015-04-06 15:15:52 -0400

    lenses

--- src/Lens.hs
+++ src/Lens.hs
@@ -27,17 +27,17 @@ type Lens' f s a = Lens f s s a a
 lens :: Functor f => (s -> a) -> (s -> b -> t) -> Lens f s t a b
 lens get' set' f s = set' s <$> f (get' s)
 
-get :: ((a -> Const a b) -> s -> Const a t) -> s -> a
+get :: Lens (Const a) s t a b -> s -> a
 get l = getConst . l Const
 
-(^.) :: s -> ((a -> Const a b) -> s -> Const a t) -> a
+(^.) :: s -> Lens (Const a) s t a b -> a
 (^.) = flip get
 
-set :: ((a -> Identity b) -> s -> Identity t) -> b -> s -> t
+set :: Lens Identity s t a b -> b -> s -> t
 set l b = runIdentity . l (const $ Identity b)
 
-(.~) :: ((a -> Identity b) -> s -> Identity t) -> b -> s -> t
+(.~) :: Lens Identity s t a b -> b -> s -> t
 (.~) = set
 
-(+~) :: Num a => ((a -> Identity a) -> s -> Identity t) -> a -> s -> t
+(+~) :: Num a => Lens Identity s t a a -> a -> s -> t
 (+~) l a = runIdentity . l (Identity . (+ a))
