0779649674fc9a242dcafec3b1ed761a57a90f08
Andy Sonnenburg <andy.sonnenburg@mathworks.com>
2015-04-10 17:53:57 -0400

    cleanup

--- src/Path.hs
+++ src/Path.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 module Path
        ( Path
@@ -13,14 +15,16 @@ module Path
 import Control.Applicative
 import Control.Monad
 import Data.Bool
+import Data.Bits
 import Data.Eq
 import Data.Foldable
 import Data.Function
 import Data.Functor.Identity
+import Data.Traversable
 import Data.Maybe (Maybe (..))
 import Data.Monoid
 import Data.Ord
-import Prelude (Num (..), Int, div, seq)
+import Prelude (Num (..), Int, div, seq, subtract)
 import Text.Show
 
 -- $setup
@@ -45,7 +49,7 @@ import Text.Show
 
 data Path a
   = Cons {-# UNPACK #-} !Int (Tree a) (Path a)
-  | Nil deriving Eq
+  | Nil deriving (Eq, Functor, Traversable)
 
 instance Show a => Show (Path a) where
   showsPrec p xs =
@@ -59,11 +63,6 @@ instance Show a => Show (Path a) where
 instance Monoid (Path a) where
   mempty = Nil
   mappend = flip (foldr cons)
-
-instance Functor Path where
-  fmap f = \ case
-    Cons n_t t xs -> Cons n_t (fmap f t) (fmap f xs)
-    Nil -> Nil
 
 -- $foldable
 -- prop> null (xs :: String) == null (Path.fromList xs)
@@ -82,17 +81,25 @@ instance Foldable Path where
 
 data Tree a
   = Branch a (Tree a) (Tree a)
-  | Leaf a deriving Eq
+  | Leaf a deriving (Eq, Functor, Traversable)
 
-instance Functor Tree where
-  fmap f = \ case
-    Branch x t1 t2 -> Branch (f x) (fmap f t1) (fmap f t2)
-    Leaf x -> Leaf (f x)
+-- $foldable
+-- >>> length (Leaf 'a')
+-- 1
+-- >>> length (Branch 'a' (Leaf 'b') (Leaf 'c'))
+-- 3
+-- >>> null (Leaf 'a')
+-- False
+-- >>> null (Branch 'a' (Leaf 'b') (Leaf 'c'))
+-- False
 
 instance Foldable Tree where
   foldMap f = \ case
-    Branch x t1 t2 -> f x <> foldMap f t1 <> foldMap f t2
+    Branch x t_1 t_2 -> f x <> foldMap f t_1 <> foldMap f t_2
     Leaf x -> f x
+  length = subtract 1 . unsafeShiftL 1 . fix (\ rec' n -> \ case
+    Branch _ t_1 _ -> let n' = n + 1 in n' `seq` rec' n' t_1
+    Leaf _ -> n) 1
   null =
     const False
 
@@ -101,8 +108,8 @@ instance Foldable Tree where
 -- prop> Path.fromList (x:xs :: String) == Path.cons x (Path.fromList xs)
 cons :: a -> Path a -> Path a
 cons x xs = case xs of
-  Cons n_t1 t1 (Cons n_t2 t2 ys)
-    | n_t1 == n_t2 -> Cons (n_t1 + n_t2 + 1) (Branch x t1 t2) ys
+  Cons n_t_1 t_1 (Cons n_t_2 t_2 ys)
+    | n_t_1 == n_t_2 -> Cons (n_t_1 + n_t_2 + 1) (Branch x t_1 t_2) ys
   _ -> Cons 1 (Leaf x) xs
 
 -- |
@@ -114,7 +121,7 @@ fromList = foldr cons mempty
 -- prop> uncons (xs :: String) == (fmap.fmap) toList (Path.uncons (Path.fromList xs))
 uncons :: Path a -> Maybe (a, Path a)
 uncons = \ case
-  Cons n_t (Branch x t1 t2) xs -> Just (x, consTrees (n_t `div` 2) t1 t2 xs)
+  Cons n_t (Branch x t_1 t_2) xs -> Just (x, consTrees (n_t `div` 2) t_1 t_2 xs)
   Cons _ (Leaf x) xs -> Just (x, xs)
   Nil -> Nothing
 
@@ -130,18 +137,18 @@ drop i xs = i `seq` case xs of
   _ -> xs
 
 dropTree :: Int -> Int -> Tree a -> Path a -> Path a
-dropTree i n_t (Branch _ t1 t2) xs
-  | i == 1 = consTrees n_t' t1 t2 xs
+dropTree i n_t (Branch _ t_1 t_2) xs
+  | i == 1 = consTrees n_t' t_1 t_2 xs
   | otherwise = case compare i (n_t' + 1) of
-    LT -> dropTree (i - 1) n_t' t1 (Cons n_t' t2 xs)
-    EQ -> Cons n_t' t2 xs
-    GT -> dropTree (i - n_t' - 1) n_t' t2 xs
+    LT -> dropTree (i - 1) n_t' t_1 (Cons n_t' t_2 xs)
+    EQ -> Cons n_t' t_2 xs
+    GT -> dropTree (i - n_t' - 1) n_t' t_2 xs
   where
     n_t' = n_t `div` 2
 dropTree _ _ _ xs = xs
 
 -- |
--- prop> lca (xs :: String) ys == toList (Path.lca (Path.fromList xs) (Path.fromList ys))
+-- prop> \ (Ordered xs) (Ordered ys) -> lca (xs :: String) ys == toList (Path.lca (Path.fromList xs) (Path.fromList ys))
 lca :: Eq a => Path a -> Path a -> Path a
 lca xs ys = runIdentity $ lcaM (\ x y -> Identity $ x == y) xs ys
 
@@ -156,9 +163,9 @@ lcaM f xs ys =
 dropWhileM2 :: Monad m => (a -> b -> m Bool) -> Path a -> Path b -> m (Path a)
 dropWhileM2 f xs@(Cons n_t t_x xs') (Cons _ t_y ys') =
   ifM
-  (zipRootWith f t_x t_y)
+  (foldRoot2 f t_x t_y)
   (ifM
-   (zipHeadWithA f xs' ys')
+   (foldHeadA2 f xs' ys')
    (dropWhileM2 f xs' ys')
    (dropTreeWhileM2 f n_t t_x t_y xs'))
   (pure xs)
@@ -168,9 +175,9 @@ dropWhileM2 _ _ _ =
 dropTreeWhileM2 :: Monad m => (a -> b -> m Bool) -> Int -> Tree a -> Tree b -> Path a -> m (Path a)
 dropTreeWhileM2 f n_t (Branch _ t_x_1 t_x_2) (Branch _ t_y_1 t_y_2) xs =
   ifM
-  (zipRootWith f t_x_1 t_y_1)
+  (foldRoot2 f t_x_1 t_y_1)
   (ifM
-   (zipRootWith f t_x_2 t_y_2)
+   (foldRoot2 f t_x_2 t_y_2)
    (dropTreeWhileM2 f n_t' t_x_2 t_y_2 xs)
    (dropTreeWhileM2 f n_t' t_x_1 t_y_1 (Cons n_t' t_x_2 xs)))
   (pure $ consTrees n_t' t_x_1 t_x_2 xs)
@@ -179,17 +186,17 @@ dropTreeWhileM2 f n_t (Branch _ t_x_1 t_x_2) (Branch _ t_y_1 t_y_2) xs =
 dropTreeWhileM2 _ _ _ _ xs = pure xs
 
 consTrees :: Int -> Tree a -> Tree a -> Path a -> Path a
-consTrees n_t t1 t2 xs = Cons n_t t1 (Cons n_t t2 xs)
+consTrees n_t t_1 t_2 xs = Cons n_t t_1 (Cons n_t t_2 xs)
 
 ifM :: Monad m => m Bool -> m a -> m a -> m a
 ifM p x y = p >>= bool y x
 
-zipHeadWithA :: Applicative f => (a -> b -> f Bool) -> Path a -> Path b -> f Bool
-zipHeadWithA f (Cons _ xs _) (Cons _ ys _) = zipRootWith f xs ys
-zipHeadWithA _ _ _ = pure False
+foldHeadA2 :: Applicative f => (a -> b -> f Bool) -> Path a -> Path b -> f Bool
+foldHeadA2 f (Cons _ xs _) (Cons _ ys _) = foldRoot2 f xs ys
+foldHeadA2 _ _ _ = pure False
 
-zipRootWith :: (a -> b -> c) -> Tree a -> Tree b -> c
-zipRootWith f xs ys = f (root xs) (root ys)
+foldRoot2 :: (a -> b -> c) -> Tree a -> Tree b -> c
+foldRoot2 f xs ys = f (root xs) (root ys)
 
 root :: Tree a -> a
 root (Branch x _ _) = x
